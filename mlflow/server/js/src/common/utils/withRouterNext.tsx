af17f04efcf2412fd3f5995f796fc88e3a02cc8e
namedProfile <namedProfile.profiles>
2025-03-07 02:44:45 +0000

    type script errro, trying to fix. Generics Mismatch, Type Assertions and Incorrect Prop Spreading

--- mlflow/server/js/src/common/utils/withRouterNext.tsx
+++ mlflow/server/js/src/common/utils/withRouterNext.tsx
@@ -17,21 +17,59 @@ export interface WithRouterNextProps<Params extends RouterDOMParams = RouterDOMP
  * This HoC serves as a retrofit for class components enabling them to use
  * react-router v6's location, navigate, and params being injected via props.
  */
-export const withRouterNext =
-  <T, Props extends WithRouterNextProps>(
-    Component: React.ComponentType<T & WithRouterNextProps>,
-  ) =>
-  (props: Omit<Props, keyof WithRouterNextProps>) => {
+export const withRouterNext = <T extends {}>(
+  Component: React.ComponentType<T & WithRouterNextProps>,
+) => {
+  return (props: Omit<T, keyof WithRouterNextProps>) => {
     const location = useLocation();
     const navigate = useNavigate();
     const params = useParams<RouterDOMParams>();
 
-    return (
-      <Component
-        {...(props as Props)}
-        params={params as RouterDOMParams}
-        location={location}
-        navigate={navigate}
-      />
-    );
+    // Combine the props with the router-specific props
+    const componentProps = {
+      ...props,
+      params,
+      location,
+      navigate,
+    } as T & WithRouterNextProps;
+
+    return <Component {...componentProps} />;
   };
+};
+// import React from 'react';
+// import {
+//   type Location,
+//   type Params as RouterDOMParams,
+//   useLocation,
+//   useNavigate,
+//   useParams,
+// } from './RoutingUtils';
+
+// export interface WithRouterNextProps<Params extends RouterDOMParams = RouterDOMParams> {
+//   navigate: ReturnType<typeof useNavigate>;
+//   location: Location;
+//   params: Params;
+// }
+
+// /**
+//  * This HoC serves as a retrofit for class components enabling them to use
+//  * react-router v6's location, navigate, and params being injected via props.
+//  */
+// export const withRouterNext =
+//   <T, Props extends WithRouterNextProps>(
+//     Component: React.ComponentType<T & WithRouterNextProps>,
+//   ) =>
+//   (props: Omit<Props, keyof WithRouterNextProps>) => {
+//     const location = useLocation();
+//     const navigate = useNavigate();
+//     const params = useParams<RouterDOMParams>();
+
+//     return (
+//       <Component
+//         {...(props as Props)}
+//         params={params as RouterDOMParams}
+//         location={location}
+//         navigate={navigate}
+//       />
+//     );
+//   };
